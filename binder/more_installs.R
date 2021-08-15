347b4121642f38a981f51d7a9ecadc4a4737cac9
text-utl <lambamanika07@gmail.com>
2021-08-15 21:24:29 +0530

    binder update

--- binder/more_installs.R
+++
@@ -1,3 +0,0 @@
-# Specifying the mirrors is based on https://stackoverflow.com/a/11488224/8508004 and was added because 
-# without them R complained
-install.packages("githubinstall", repos='http://cran.us.r-project.org')
---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("githubinstall")
+library(githubinstall)
+githubinstall("textnets")
